f2a52b9d8851826c5f317e1f06b5cdcd7e81375a
2483505 <TR.Praharshini@cognizant.com>
2026-06-02 09:51:07 +0530

    Changes in kpi and compliance logic also inventory and sale repository.

--- src/main/java/com/project/retailproject/config/SecurityConfig.java
+++ src/main/java/com/project/retailproject/config/SecurityConfig.java
@@ -36,9 +36,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
 
-                        .requestMatchers("/api/compliance-reports/**").permitAll()
                         .requestMatchers("/api/kpi-reports/**").permitAll()
-
+                        .requestMatchers("/api/compliance-reports/**").permitAll()
 
                         .requestMatchers("/api/sales/**").hasAnyRole("ADMIN", "STORE_ASSOCIATE", "STORE_MANAGER")
 
